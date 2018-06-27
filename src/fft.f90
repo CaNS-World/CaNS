a8424a5da8484537855fc833a71e365af4ff0d5f
Pedro Costa <p.simoes.costa@gmail.com>
2018-06-27 09:41:17 +0200

    asthetic indentation changes

--- src/fft.f90
+++ src/fft.f90
@@ -27,7 +27,7 @@ module mod_fft
     integer :: ix,iy
     !$ call dfftw_init_threads(ierr)
     !$ call dfftw_plan_with_nthreads(omp_get_max_threads())
-
+    !
     ! fft in x
     !
     ! prepare plans with guru interface
@@ -89,19 +89,19 @@ module mod_fft
   subroutine fftend(arrplan)
     implicit none
     type(C_PTR), intent(in), dimension(2,2) :: arrplan
-      call dfftw_destroy_plan(arrplan(1,1))
-      call dfftw_destroy_plan(arrplan(1,2))
-      call dfftw_destroy_plan(arrplan(2,1))
-      call dfftw_destroy_plan(arrplan(2,2))
-      !$call dfftw_cleanup_threads(ierr)
-      return
+    call dfftw_destroy_plan(arrplan(1,1))
+    call dfftw_destroy_plan(arrplan(1,2))
+    call dfftw_destroy_plan(arrplan(2,1))
+    call dfftw_destroy_plan(arrplan(2,2))
+    !$call dfftw_cleanup_threads(ierr)
+    return
   end subroutine fftend
   !
   subroutine fftd(plan,arr)
     implicit none
     type(C_PTR), intent(in) :: plan 
     real(8), intent(inout), dimension(:,:,:) :: arr
-      call dfftw_execute_r2r(plan,arr,arr)
+    call dfftw_execute_r2r(plan,arr,arr)
     return
   end subroutine fftd
   !
@@ -109,7 +109,7 @@ module mod_fft
     implicit none
     type(C_PTR), intent(in) :: plan 
     real(8), intent(inout), dimension(:,:,:) :: arr
-      call dfftw_execute_r2r(plan,arr,arr)
+    call dfftw_execute_r2r(plan,arr,arr)
     return
   end subroutine ffti
   !
@@ -119,53 +119,53 @@ module mod_fft
   character(len=1), intent(in) :: c_or_f
   integer, intent(out) :: kind_fwd,kind_bwd
   real(8), intent(out), dimension(2) :: norm
-    if(c_or_f.eq.'c') then
-      select case(bc(0)//bc(1))
-      case('PP')
-        kind_fwd = FFTW_R2HC
-        kind_bwd = FFTW_HC2R
-        norm = (/1.d0,0.d0/)
-      case('NN')
-        kind_fwd = FFTW_REDFT10
-        kind_bwd = FFTW_REDFT01
-        norm = (/2.d0,0.d0/)
-      case('DD')
-        kind_fwd = FFTW_RODFT10
-        kind_bwd = FFTW_RODFT01
-        norm = (/2.d0,0.d0/)
-      case('ND')
-        kind_fwd = FFTW_REDFT11
-        kind_bwd = FFTW_REDFT11
-        norm = (/2.d0,0.d0/)
-      case('DN')
-        kind_fwd = FFTW_RODFT11
-        kind_bwd = FFTW_RODFT11
-        norm = (/2.d0,0.d0/)
-      end select
-    elseif(c_or_f.eq.'f') then
-      select case(bc(0)//bc(1))
-      case('PP')
-        kind_fwd = FFTW_R2HC
-        kind_bwd = FFTW_HC2R
-        norm = (/1.d0,0.d0/)
-      case('NN')
-        kind_fwd = FFTW_REDFT00
-        kind_bwd = FFTW_REDFT00
-        norm = (/2.d0,-1.d0/)
-      case('DD')
-        kind_fwd = FFTW_RODFT00
-        kind_bwd = FFTW_RODFT00
-        norm = (/2.d0,1.d0/)
-      case('ND')
-        kind_fwd = FFTW_REDFT10
-        kind_bwd = FFTW_REDFT01
-        norm = (/2.d0,0.d0/)
-      case('DN')
-        kind_fwd = FFTW_RODFT01
-        kind_bwd = FFTW_RODFT10
-        norm = (/2.d0,0.d0/)
-      end select
-    endif
+  if(c_or_f.eq.'c') then
+    select case(bc(0)//bc(1))
+    case('PP')
+      kind_fwd = FFTW_R2HC
+      kind_bwd = FFTW_HC2R
+      norm = (/1.d0,0.d0/)
+    case('NN')
+      kind_fwd = FFTW_REDFT10
+      kind_bwd = FFTW_REDFT01
+      norm = (/2.d0,0.d0/)
+    case('DD')
+      kind_fwd = FFTW_RODFT10
+      kind_bwd = FFTW_RODFT01
+      norm = (/2.d0,0.d0/)
+    case('ND')
+      kind_fwd = FFTW_REDFT11
+      kind_bwd = FFTW_REDFT11
+      norm = (/2.d0,0.d0/)
+    case('DN')
+      kind_fwd = FFTW_RODFT11
+      kind_bwd = FFTW_RODFT11
+      norm = (/2.d0,0.d0/)
+    end select
+  elseif(c_or_f.eq.'f') then
+    select case(bc(0)//bc(1))
+    case('PP')
+      kind_fwd = FFTW_R2HC
+      kind_bwd = FFTW_HC2R
+      norm = (/1.d0,0.d0/)
+    case('NN')
+      kind_fwd = FFTW_REDFT00
+      kind_bwd = FFTW_REDFT00
+      norm = (/2.d0,-1.d0/)
+    case('DD')
+      kind_fwd = FFTW_RODFT00
+      kind_bwd = FFTW_RODFT00
+      norm = (/2.d0,1.d0/)
+    case('ND')
+      kind_fwd = FFTW_REDFT10
+      kind_bwd = FFTW_REDFT01
+      norm = (/2.d0,0.d0/)
+    case('DN')
+      kind_fwd = FFTW_RODFT01
+      kind_bwd = FFTW_RODFT10
+      norm = (/2.d0,0.d0/)
+    end select
+  endif
   return
   end subroutine find_fft
 end module mod_fft
