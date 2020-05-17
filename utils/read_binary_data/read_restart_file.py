1063219016240617a48621c5388841e3afdcac31
Pedro Costa <p.simoes.costa@gmail.com>
2020-05-17 13:19:48 +0000

    added scripts that read output binary data using python

---
+++ utils/read_binary_data/read_restart_file.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import numpy as np
+#
+# setting up some parameters
+#
+iprecision = 8                # precision of the real-valued data
+r0 = np.array([0.,0.,0.]) # domain origin
+non_uniform_grid = True
+#
+# read geometry file
+#
+geofile  = "geometry.out"
+data = np.loadtxt(geofile, comments = "!", max_rows = 2)
+ng = data[0,:].astype('int')
+l  = data[1,:]
+dl = l/(1.*ng)
+#
+# read and generate grid
+#
+xp = np.arange(r0[0]+dl[0]/2.,r0[0]+l[0],dl[0]) # centered  x grid
+yp = np.arange(r0[1]+dl[1]/2.,r0[1]+l[1],dl[1]) # centered  y grid
+zp = np.arange(r0[2]+dl[2]/2.,r0[2]+l[2],dl[2]) # centered  z grid
+xu = xp + dl[0]/2.                              # staggered x grid
+yv = yp + dl[1]/2.                              # staggered y grid
+zw = zp + dl[2]/2.                              # staggered z grid
+if(non_uniform_grid):
+    f   = open('grid.bin','rb')
+    if(    iprecision == 4):
+        grid_z = np.fromfile(f,dtype='float32')
+    else:
+        grid_z = np.fromfile(f,dtype='float64')
+    f.close()
+    grid_z = np.reshape(grid_z,(ng[2],4),order='F')
+    zp = r0[2] + grid_z[:,2] # centered  z grid
+    zw = r0[2] + grid_z[:,1] # staggered z grid
+#
+# read checkpoint binary file
+#
+filenamei = input("Name of the binary restart file written by CaNS [fld.bin]: ") or "fld.bin"
+offset     = 0
+disp       = np.prod(ng)
+data       = np.zeros([ng[0],ng[1],ng[2],4]) # u[:,:,:],v[:,:,:],w[:,:,:],p[:,:,:]
+fldinfo    = np.zeros([2])
+with open(filenamei,'rb') as f:
+    for p in range(4):
+        f.seek(offset)
+        fld = np.fromfile(f,dtype='float64',count=disp)
+        data[:,:,:,p] = np.reshape(fld,(ng[0],ng[1],ng[2]),order='F')
+        offset += iprecision*disp
+    f.seek(offset)
+    fldinfo[:] = np.fromfile(f,dtype='float64',count=2)
+f.close
+#
+# store data in arrays
+#
+u = data[:,:,:,0]
+v = data[:,:,:,1]
+w = data[:,:,:,2]
+p = data[:,:,:,3]
+time  =     fldinfo[0]
+istep = int(fldinfo[1])
---
+++ utils/read_binary_data/read_single_field_binary.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import numpy as np
+#
+# setting up some parameters
+#
+iprecision = 8                # precision of the real-valued data
+r0 = np.array([0.,0.,0.]) # domain origin
+non_uniform_grid = True
+#
+# read geometry file
+#
+geofile  = "geometry.out"
+geo = np.loadtxt(geofile, comments = "!", max_rows = 2)
+ng = geo[0,:].astype('int')
+l  = geo[1,:]
+dl = l/(1.*ng)
+#
+# read and generate grid
+#
+xp = np.arange(r0[0]+dl[0]/2.,r0[0]+l[0],dl[0]) # centered  x grid
+yp = np.arange(r0[1]+dl[1]/2.,r0[1]+l[1],dl[1]) # centered  y grid
+zp = np.arange(r0[2]+dl[2]/2.,r0[2]+l[2],dl[2]) # centered  z grid
+xu = xp + dl[0]/2.                              # staggered x grid
+yv = yp + dl[1]/2.                              # staggered y grid
+zw = zp + dl[2]/2.                              # staggered z grid
+if(non_uniform_grid):
+    f   = open('grid.bin','rb')
+    if(    iprecision == 4):
+        grid_z = np.fromfile(f,dtype='float32')
+    else:
+        grid_z = np.fromfile(f,dtype='float64')
+    f.close()
+    grid_z = np.reshape(grid_z,(ng[2],4),order='F')
+    zp = r0[2] + grid_z[:,2] # centered  z grid
+    zw = r0[2] + grid_z[:,1] # staggered z grid
+#
+# read binary file
+#
+filenamei   = input("Name of the binary file written by CaNS (e.g. vex_fld_0000000.bin)]: ")
+iskipx      = input("Data saved every (ix, iy, iz) points. Value of ix? [1]: ") or "1"
+iskipy      = input("Data saved every (ix, iy, iz) points. Value of iy? [1]: ") or "1"
+iskipz      = input("Data saved every (ix, iy, iz) points. Value of iz? [1]: ") or "1"
+iskip       = np.array([iskipx,iskipy,iskipz]).astype(int)
+n           = (ng[:]/iskip[:]).astype(int)
+data        = np.zeros([n[0],n[1],n[2]]) # u[:,:,:],v[:,:,:],w[:,:,:],p[:,:,:]
+fld         = np.fromfile(filenamei,dtype='float64')
+f.close
+data[:,:,:] = np.reshape(fld,(ng[0],ng[1],ng[2]),order='F')
