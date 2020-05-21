5cd304c7bf8def6d0b4cb740d954917accb0fea3
Pedro Costa <p.simoes.costa@gmail.com>
2020-05-21 14:48:39 +0000

    changes in chkdt that Nicolo Scapin requested feverously

--- src/chkdt.f90
+++ src/chkdt.f90
@@ -20,7 +20,7 @@ module mod_chkdt
     real(rp) :: dxi,dyi,dzi
     real(rp) :: ux,uy,uz,vx,vy,vz,wx,wy,wz
     real(rp) :: dtix,dtiy,dtiz,dti,dlmin
-    integer :: i,j,k
+    integer :: i,j,k,im,ip,jm,jp,km,kp
     !
     dti = 0.
     dxi = 1./dl(1)
@@ -28,21 +28,28 @@ module mod_chkdt
     dzi = 1./dl(3)
     !$OMP PARALLEL DO DEFAULT(none) &
     !$OMP SHARED(n,u,v,w,dxi,dyi,dzi,dzci,dzfi) &
-    !$OMP PRIVATE(i,j,k,ux,uy,uz,vx,vy,vz,wx,wy,wz,dtix,dtiy,dtiz) &
+    !$OMP PRIVATE(i,j,k,im,jm,km,ip,jp,kp) &
+    !$OMP PRIVATE(ux,uy,uz,vx,vy,vz,wx,wy,wz,dtix,dtiy,dtiz) &
     !$OMP REDUCTION(max:dti)
     do k=1,n(3)
+      kp = k + 1
+      km = k - 1
       do j=1,n(2)
+        jp = j + 1
+        jm = j - 1
         do i=1,n(1)
+          ip = i + 1
+          im = i - 1
           ux = abs(u(i,j,k))
-          vx = 0.25*abs( v(i,j,k)+v(i,j-1,k)+v(i+1,j,k)+v(i+1,j-1,k) )
-          wx = 0.25*abs( w(i,j,k)+w(i,j,k-1)+w(i+1,j,k)+w(i+1,j,k-1) )
+          vx = 0.25*abs( v(i,j,k)+v(i,jm,k)+v(ip,j,k)+v(ip,jm,k) )
+          wx = 0.25*abs( w(i,j,k)+w(i,j,km)+w(ip,j,k)+w(ip,j,km) )
           dtix = ux*dxi+vx*dyi+wx*dzfi(k)
-          uy = 0.25*abs( u(i,j,k)+u(i,j+1,k)+u(i-1,j+1,k)+u(i-1,j,k) )
+          uy = 0.25*abs( u(i,j,k)+u(i,jp,k)+u(im,jp,k)+u(im,j,k) )
           vy = abs(v(i,j,k))
-          wy = 0.25*abs( w(i,j,k)+w(i,j+1,k)+w(i,j+1,k-1)+w(i,j,k-1) )
+          wy = 0.25*abs( w(i,j,k)+w(i,jp,k)+w(i,jp,km)+w(i,j,km) )
           dtiy = uy*dxi+vy*dyi+wy*dzfi(k)
-          uz = 0.25*abs( u(i,j,k)+u(i-1,j,k)+u(i-1,j,k+1)+u(i,j,k+1) )
-          vz = 0.25*abs( v(i,j,k)+v(i,j-1,k)+v(i,j-1,k+1)+v(i,j,k+1) )
+          uz = 0.25*abs( u(i,j,k)+u(im,j,k)+u(im,j,kp)+u(i,j,kp) )
+          vz = 0.25*abs( v(i,j,k)+v(i,jm,k)+v(i,jm,kp)+v(i,j,kp) )
           wz = abs(w(i,j,k))
           dtiz = uz*dxi+vz*dyi+wz*dzci(k)
           dti = max(dti,dtix,dtiy,dtiz)
