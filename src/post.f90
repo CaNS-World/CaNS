3814de145393e928468500c70c8bbdf1f38bd79e
Pedro Costa <p.simoes.costa@gmail.com>
2021-10-27 17:32:28 +0000

    fixed routines for non-uniform grid spacing

--- src/post.f90
+++ src/post.f90
@@ -1,6 +1,5 @@
 module mod_post
   use mod_types
-  !@cuf use cudafor
   implicit none
   private
   public vorticity, rotation_rate, strain_rate, q_criterion
@@ -14,20 +13,12 @@ contains
     real(rp), intent(out), dimension(1:,1:,1:) :: vox,voy,voz
     real(rp) :: dxi,dyi
     integer :: i,j,k
-#ifdef USE_CUDA
-    integer :: istat
-    attributes(managed) :: dzci,ux,uy,uz,vox,voy,voz
-#endif
     dxi = dli(1)
     dyi = dli(2)
-#ifdef USE_CUDA
-    !$cuf kernel do(3) <<<*,*>>>
-#else
     !$OMP PARALLEL DEFAULT(none) &
     !$OMP SHARED(n,dxi,dyi,dzci,ux,uy,uz,vox,voy,voz) &
     !$OMP PRIVATE(i,j,k)
     !$OMP DO
-#endif
     do k=1,n(3)
        do j=1,n(2)
           do i=1,n(1)
@@ -35,19 +26,19 @@ contains
              ! x component of the vorticity at cell center
              !
              vox(i,j,k) = 0.25_rp*( &
-                                   (uz(i,j+1,k  )-uz(i,j  ,k  ))*dyi - (uy(i,j  ,k+1)-uy(i,j  ,k  ))*dzci(k) + &
-                                   (uz(i,j+1,k-1)-uz(i,j  ,k-1))*dyi - (uy(i,j  ,k  )-uy(i,j  ,k-1))*dzci(k) + &
-                                   (uz(i,j  ,k  )-uz(i,j-1,k  ))*dyi - (uy(i,j-1,k+1)-uy(i,j-1,k  ))*dzci(k) + &
-                                   (uz(i,j  ,k-1)-uz(i,j-1,k-1))*dyi - (uy(i,j-1,k  )-uy(i,j-1,k-1))*dzci(k) &
+                                   (uz(i,j+1,k  )-uz(i,j  ,k  ))*dyi - (uy(i,j  ,k+1)-uy(i,j  ,k  ))*dzci(k  ) + &
+                                   (uz(i,j+1,k-1)-uz(i,j  ,k-1))*dyi - (uy(i,j  ,k  )-uy(i,j  ,k-1))*dzci(k-1) + &
+                                   (uz(i,j  ,k  )-uz(i,j-1,k  ))*dyi - (uy(i,j-1,k+1)-uy(i,j-1,k  ))*dzci(k  ) + &
+                                   (uz(i,j  ,k-1)-uz(i,j-1,k-1))*dyi - (uy(i,j-1,k  )-uy(i,j-1,k-1))*dzci(k-1) &
                                   )
              !
              ! y component of the vorticity at cell center
              !
              voy(i,j,k) = 0.25_rp*( &
-                                   (ux(i  ,j,k+1)-ux(i  ,j,k  ))*dzci(k) - (uz(i+1,j,k  )-uz(i  ,j,k  ))*dxi + &
-                                   (ux(i  ,j,k  )-ux(i  ,j,k-1))*dzci(k) - (uz(i+1,j,k-1)-uz(i  ,j,k-1))*dxi + &
-                                   (ux(i-1,j,k+1)-ux(i-1,j,k  ))*dzci(k) - (uz(i  ,j,k  )-uz(i-1,j,k  ))*dxi + &
-                                   (ux(i-1,j,k  )-ux(i-1,j,k-1))*dzci(k) - (uz(i  ,j,k-1)-uz(i-1,j,k-1))*dxi &
+                                   (ux(i  ,j,k+1)-ux(i  ,j,k  ))*dzci(k  ) - (uz(i+1,j,k  )-uz(i  ,j,k  ))*dxi + &
+                                   (ux(i  ,j,k  )-ux(i  ,j,k-1))*dzci(k-1) - (uz(i+1,j,k-1)-uz(i  ,j,k-1))*dxi + &
+                                   (ux(i-1,j,k+1)-ux(i-1,j,k  ))*dzci(k  ) - (uz(i  ,j,k  )-uz(i-1,j,k  ))*dxi + &
+                                   (ux(i-1,j,k  )-ux(i-1,j,k-1))*dzci(k-1) - (uz(i  ,j,k-1)-uz(i-1,j,k-1))*dxi &
                                   )
              !
              ! z component of the vorticity at cell center
@@ -61,44 +52,33 @@ contains
           enddo
        enddo
     enddo
-    !@cuf istat=cudaDeviceSynchronize()
-#ifndef USE_CUDA
     !$OMP END PARALLEL
-#endif
   end subroutine vorticity
-  subroutine strain_rate(n,dli,dzci,ux,uy,uz,str)
+  subroutine strain_rate(n,dli,dzci,dzfi,ux,uy,uz,str)
     implicit none
     integer , intent(in ), dimension(3)        :: n
     real(rp), intent(in ), dimension(3)        :: dli
-    real(rp), intent(in ), dimension(0:)       :: dzci
+    real(rp), intent(in ), dimension(0:)       :: dzci,dzfi
     real(rp), intent(in ), dimension(0:,0:,0:) :: ux,uy,uz
     real(rp), intent(out), dimension(1:,1:,1:) :: str
     real(rp) :: s11,s22,s33,s12,s13,s23
     real(rp) :: dxi,dyi
     integer :: i,j,k
-#ifdef USE_CUDA
-    integer :: istat
-    attributes(managed) :: dzci,ux,uy,uz,str
-#endif
     dxi = dli(1)
     dyi = dli(2)
     !
     ! compute sijsij, where sij = (1/2)(du_i/dx_j + du_j/dx_i)
     !
-#ifdef USE_CUDA
-    !$cuf kernel do(3) <<<*,*>>>
-#else
     !$OMP PARALLEL DEFAULT(none) &
-    !$OMP SHARED(n,dxi,dyi,dzci,ux,uy,uz,str) &
+    !$OMP SHARED(n,dxi,dyi,dzci,dzfi,ux,uy,uz,str) &
     !$OMP PRIVATE(i,j,k,s11,s12,s13,s22,s23,s33)
     !$OMP DO
-#endif
     do k=1,n(3)
        do j=1,n(2)
           do i=1,n(1)
              s11 = ((ux(i,j,k)-ux(i-1,j,k))*dxi)**2
              s22 = ((uy(i,j,k)-uy(i,j-1,k))*dyi)**2
-             s33 = ((uz(i,j,k)-uz(i,j,k-1))*dzci(k))**2
+             s33 = ((uz(i,j,k)-uz(i,j,k-1))*dzfi(k))**2
              s12 = .25_rp*( &
                            ((ux(i  ,j+1,k)-ux(i  ,j  ,k))*dyi + (uy(i+1,j  ,k)-uy(i  ,j  ,k))*dxi)**2 + &
                            ((ux(i  ,j  ,k)-ux(i ,j-1 ,k))*dyi + (uy(i+1,j-1,k)-uy(i  ,j-1,k))*dxi)**2 + &
@@ -106,25 +86,22 @@ contains
                            ((ux(i-1,j  ,k)-ux(i-1,j-1,k))*dyi + (uy(i  ,j-1,k)-uy(i-1,j-1,k))*dxi)**2 &
                           )*.25_rp
              s13 = .25_rp*( &
-                            ((ux(i  ,j,k+1)-ux(i  ,j,k  ))*dzci(k) + (uz(i+1,j,k  )-uz(i  ,j,k  ))*dxi)**2 + &
-                            ((ux(i  ,j,k  )-ux(i  ,j,k-1))*dzci(k) + (uz(i+1,j,k-1)-uz(i  ,j,k-1))*dxi)**2 + &
-                            ((ux(i-1,j,k+1)-ux(i-1,j,k  ))*dzci(k) + (uz(i  ,j,k  )-uz(i-1,j,k  ))*dxi)**2 + &
-                            ((ux(i-1,j,k  )-ux(i-1,j,k-1))*dzci(k) + (uz(i  ,j,k-1)-uz(i-1,j,k-1))*dxi)**2 &
+                            ((ux(i  ,j,k+1)-ux(i  ,j,k  ))*dzci(k  ) + (uz(i+1,j,k  )-uz(i  ,j,k  ))*dxi)**2 + &
+                            ((ux(i  ,j,k  )-ux(i  ,j,k-1))*dzci(k-1) + (uz(i+1,j,k-1)-uz(i  ,j,k-1))*dxi)**2 + &
+                            ((ux(i-1,j,k+1)-ux(i-1,j,k  ))*dzci(k  ) + (uz(i  ,j,k  )-uz(i-1,j,k  ))*dxi)**2 + &
+                            ((ux(i-1,j,k  )-ux(i-1,j,k-1))*dzci(k-1) + (uz(i  ,j,k-1)-uz(i-1,j,k-1))*dxi)**2 &
                           )*.25_rp
              s23 = .25_rp*( &
-                           ((uy(i,j  ,k+1)-uy(i,j  ,k  ))*dzci(k) + (uz(i,j+1,k  )-uz(i,j  ,k  ))*dyi)**2 + &
-                           ((uy(i,j  ,k  )-uy(i,j  ,k-1))*dzci(k) + (uz(i,j+1,k-1)-uz(i,j  ,k-1))*dyi)**2 + &
-                           ((uy(i,j-1,k+1)-uy(i,j-1,k  ))*dzci(k) + (uz(i,j  ,k  )-uz(i,j-1,k  ))*dyi)**2 + &
-                           ((uy(i,j-1,k  )-uy(i,j-1,k-1))*dzci(k) + (uz(i,j  ,k-1)-uz(i,j-1,k-1))*dyi)**2 &
+                           ((uy(i,j  ,k+1)-uy(i,j  ,k  ))*dzci(k  ) + (uz(i,j+1,k  )-uz(i,j  ,k  ))*dyi)**2 + &
+                           ((uy(i,j  ,k  )-uy(i,j  ,k-1))*dzci(k-1) + (uz(i,j+1,k-1)-uz(i,j  ,k-1))*dyi)**2 + &
+                           ((uy(i,j-1,k+1)-uy(i,j-1,k  ))*dzci(k  ) + (uz(i,j  ,k  )-uz(i,j-1,k  ))*dyi)**2 + &
+                           ((uy(i,j-1,k  )-uy(i,j-1,k-1))*dzci(k-1) + (uz(i,j  ,k-1)-uz(i,j-1,k-1))*dyi)**2 &
                           )*.25_rp
              str(i,j,k) = s11+s22+s33 + 2*(s12+s13+s23)
           enddo
        enddo
     enddo
-    !@cuf istat=cudaDeviceSynchronize()
-#ifndef USE_CUDA
     !$OMP END PARALLEL
-#endif
   end subroutine strain_rate
   subroutine rotation_rate(n,dli,dzci,ux,uy,uz,ens)
     implicit none
@@ -136,23 +113,15 @@ contains
     real(rp) :: e11,e22,e33,e12,e13,e23
     real(rp) :: dxi,dyi
     integer :: i,j,k
-#ifdef USE_CUDA
-    integer :: istat
-    attributes(managed) :: dzci,ux,uy,uz,ens
-#endif
     !
     ! compute wijwij, where wij = (1/2)(du_i/dx_j - du_j/dx_i)
     !
     dxi = dli(1)
     dyi = dli(2)
-#ifdef USE_CUDA
-    !$cuf kernel do(3) <<<*,*>>>
-#else
     !$OMP PARALLEL DEFAULT(none) &
     !$OMP SHARED(n,dxi,dyi,dzci,ux,uy,uz,ens) &
     !$OMP PRIVATE(i,j,k,e12,e13,e23)
     !$OMP DO
-#endif
     do k=1,n(3)
       do j=1,n(2)
         do i=1,n(1)
@@ -163,25 +132,22 @@ contains
                         ((ux(i-1,j  ,k)-ux(i-1,j-1,k))*dyi - (uy(i  ,j-1,k)-uy(i-1,j-1,k))*dxi)**2 &
                        )*.25_rp
           e13 = .25_rp*( &
-                        ((ux(i  ,j,k+1)-ux(i  ,j,k  ))*dzci(k) - (uz(i+1,j,k  )-uz(i  ,j,k  ))*dxi)**2 + &
-                        ((ux(i  ,j,k  )-ux(i  ,j,k-1))*dzci(k) - (uz(i+1,j,k-1)-uz(i  ,j,k-1))*dxi)**2 + &
-                        ((ux(i-1,j,k+1)-ux(i-1,j,k  ))*dzci(k) - (uz(i  ,j,k  )-uz(i-1,j,k  ))*dxi)**2 + &
-                        ((ux(i-1,j,k  )-ux(i-1,j,k-1))*dzci(k) - (uz(i  ,j,k-1)-uz(i-1,j,k-1))*dxi)**2 &
+                        ((ux(i  ,j,k+1)-ux(i  ,j,k  ))*dzci(k  ) - (uz(i+1,j,k  )-uz(i  ,j,k  ))*dxi)**2 + &
+                        ((ux(i  ,j,k  )-ux(i  ,j,k-1))*dzci(k-1) - (uz(i+1,j,k-1)-uz(i  ,j,k-1))*dxi)**2 + &
+                        ((ux(i-1,j,k+1)-ux(i-1,j,k  ))*dzci(k  ) - (uz(i  ,j,k  )-uz(i-1,j,k  ))*dxi)**2 + &
+                        ((ux(i-1,j,k  )-ux(i-1,j,k-1))*dzci(k-1) - (uz(i  ,j,k-1)-uz(i-1,j,k-1))*dxi)**2 &
                        )*.25_rp
           e23 = .25_rp*( &
-                        ((uy(i,j  ,k+1)-uy(i,j  ,k  ))*dzci(k) - (uz(i,j+1,k  )-uz(i,j  ,k  ))*dyi)**2 + &
-                        ((uy(i,j  ,k  )-uy(i,j  ,k-1))*dzci(k) - (uz(i,j+1,k-1)-uz(i,j  ,k-1))*dyi)**2 + &
-                        ((uy(i,j-1,k+1)-uy(i,j-1,k  ))*dzci(k) - (uz(i,j  ,k  )-uz(i,j-1,k  ))*dyi)**2 + &
-                        ((uy(i,j-1,k  )-uy(i,j-1,k-1))*dzci(k) - (uz(i,j  ,k-1)-uz(i,j-1,k-1))*dyi)**2 &
+                        ((uy(i,j  ,k+1)-uy(i,j  ,k  ))*dzci(k  ) - (uz(i,j+1,k  )-uz(i,j  ,k  ))*dyi)**2 + &
+                        ((uy(i,j  ,k  )-uy(i,j  ,k-1))*dzci(k-1) - (uz(i,j+1,k-1)-uz(i,j  ,k-1))*dyi)**2 + &
+                        ((uy(i,j-1,k+1)-uy(i,j-1,k  ))*dzci(k  ) - (uz(i,j  ,k  )-uz(i,j-1,k  ))*dyi)**2 + &
+                        ((uy(i,j-1,k  )-uy(i,j-1,k-1))*dzci(k-1) - (uz(i,j  ,k-1)-uz(i,j-1,k-1))*dyi)**2 &
                        )*.25_rp
           ens(i,j,k) =  2._rp*(e12+e13+e23)
         enddo
       enddo
     enddo
-    !@cuf istat=cudaDeviceSynchronize()
-#ifndef USE_CUDA
     !$OMP END PARALLEL
-#endif
   end subroutine rotation_rate
   subroutine q_criterion(n,ens,str,qcr)
     implicit none
@@ -190,18 +156,10 @@ contains
     real(rp), intent(out), dimension(0:,0:,0:) :: qcr
     real(rp) :: s11,s22,s33,s12,s13,s23
     integer  :: i,j,k
-#ifdef USE_CUDA
-    integer :: istat
-    attributes(managed) :: ens,str,qcr
-#endif
-#ifdef USE_CUDA
-    !$cuf kernel do(3) <<<*,*>>>
-#else
     !$OMP PARALLEL DEFAULT(none) &
     !$OMP SHARED(n,ens,str,qcr) &
     !$OMP PRIVATE(i,j,k)
     !$OMP DO
-#endif
     do k=1,n(3)
       do j=1,n(2)
         do i=1,n(1)
@@ -209,9 +167,6 @@ contains
         enddo
       enddo
     enddo
-    !@cuf istat=cudaDeviceSynchronize()
-#ifndef USE_CUDA
     !$OMP END PARALLEL
-#endif
   end subroutine q_criterion
 end module mod_post
