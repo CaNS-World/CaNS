fbedfdb9c0183db7417af937c205ae8ce2832bab
Pedro Costa <p.simoes.costa@gmail.com>
2022-04-24 23:43:14 +0000

    added comments

--- dependencies/external.mk
+++ dependencies/external.mk
@@ -1,3 +1,6 @@
+#
+# 2decomp_fft compilation
+#
 library: $(wildcard $(LIBS_DIR)/2decomp_fft/src/*.f90)
 	cd $(LIBS_DIR)/2decomp_fft && make clean && make
 libclean: $(wildcard $(LIBS_DIR)/2decomp_fft/src/*.f90)
