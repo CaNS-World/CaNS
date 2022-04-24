600f4148fe0edeb800422d334eeafefda1b4b103
Pedro Costa <p.simoes.costa@gmail.com>
2022-04-24 18:44:29 +0000

    fixed flags and better explained build.conf

--- src/configs/flags.mk
+++ src/configs/flags.mk
@@ -13,11 +13,9 @@ ifeq ($(strip $(FFLAGS_DEBUG)),1)
 ifeq ($(strip $(FCOMP)),GNU)
 override FFLAGS += -O0 -g -fbacktrace -Wall -Wextra -pedantic -fcheck=all -finit-real=snan -ffpe-trap=invalid -std=f2018
 endif
-
 ifeq ($(strip $(FCOMP)),INTEL)
 override FFLAGS += -O0 -g -traceback -fpe0 -stand f18
 endif
-
 ifeq ($(strip $(FCOMP)),NVIDIA)
 override FFLAGS += -O0 -g -traceback -Mstandard -Minform=inform -Mbackslash -Mbounds -Mchkptr -Mchkstk
 endif
@@ -28,6 +26,12 @@ ifeq ($(strip $(FFLAGS_DEBUG_MAX)),1)
 ifeq ($(strip $(FCOMP)),GNU)
 override FFLAGS += -O0 -Wall -Wextra -Wimplicit-interface -Wno-unused-function -fPIC -g -fcheck=all -fbacktrace -ffpe-trap=invalid,zero,overflow -finit-real=snan -finit-integer=-99999999 -std=f2018
 endif
+ifeq ($(strip $(FCOMP)),INTEL)
+override FFLAGS += -O0 -g -traceback -fpe0 -stand f18
+endif
+ifeq ($(strip $(FCOMP)),NVIDIA)
+override FFLAGS += -O0 -g -traceback -Mstandard -Minform=inform -Mbackslash -Mbounds -Mchkptr -Mchkstk
+endif
 
 endif
 
@@ -36,11 +40,9 @@ ifeq ($(strip $(FFLAGS_OPT)),1)
 ifeq ($(strip $(FCOMP)),GNU)
 override FFLAGS += -O3
 endif
-
 ifeq ($(strip $(FCOMP)),INTEL)
 override FFLAGS += -O3
 endif
-
 ifeq ($(strip $(FCOMP)),NVIDIA)
 override FFLAGS += -O3
 endif
@@ -52,11 +54,9 @@ ifeq ($(strip $(FFLAGS_OPT_MAX)),1)
 ifeq ($(strip $(FCOMP)),GNU)
 override FFLAGS += -Ofast -march=native
 endif
-
 ifeq ($(strip $(FCOMP)),INTEL)
 override FFLAGS += -fast -xHost
 endif
-
 ifeq ($(strip $(FCOMP)),NVIDIA)
 override FFLAGS += -fast
 endif
@@ -75,13 +75,13 @@ endif
 ifeq ($(strip $(IMPDIFF_1D)),1)
 DEFINES += -D_IMPDIFF_1D
 endif
-ifeq ($(strip $(IMPDIFF_X)),1)
+ifeq ($(strip $(DECOMP_X)),1)
 DEFINES += -D_DECOMP_X
 endif
-ifeq ($(strip $(IMPDIFF_Y)),1)
+ifeq ($(strip $(DECOMP_Y)),1)
 DEFINES += -D_DECOMP_Y
 endif
-ifeq ($(strip $(IMPDIFF_Z)),1)
+ifeq ($(strip $(DECOMP_Z)),1)
 DEFINES += -D_DECOMP_Z
 endif
 ifeq ($(strip $(SINGLE_PRECISION)),1)
