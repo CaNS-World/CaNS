6e09816492e4e1746fbce726723c40f3a5c8cd2b
benjamic <benjamin.czaja@surf.nl>
2024-09-30 13:08:24 +0200

    adding cuDecomp specific build scripts

--- dependencies/external.mk
+++ dependencies/external.mk
@@ -4,6 +4,8 @@
 ifeq ($(strip $(GPU)),1)
 libs: $(wildcard $(LIBS_DIR)/2decomp-fft/src/*.f90)
 	cd $(LIBS_DIR)/2decomp-fft && make
+	cp $(LIBS_DIR)/cuDecomp_Makefile $(LIBS_DIR)/cuDecomp/Makefile
+	cp $(LIBS_DIR)/nvhpcsdk.conf $(LIBS_DIR)/cuDecomp/configs/
 	cd $(LIBS_DIR)/cuDecomp && make lib -j
 libsclean: $(wildcard $(LIBS_DIR)/2decomp-fft/src/*.f90)
 	cd $(LIBS_DIR)/2decomp-fft && make clean
