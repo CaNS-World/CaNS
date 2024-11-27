ba5be2508da9b0e4ffb3f1fce6be180d1263ba61
Pedro Costa <p.simoes.costa@gmail.com>
2024-11-27 15:20:03 +0000

    Fixed issue in `.xmf` file geneartor.

--- utils/visualize_fields/gen_xdmf_easy/write_xdmf.py
+++ utils/visualize_fields/gen_xdmf_easy/write_xdmf.py
@@ -42,7 +42,7 @@ saves = np.unique(saves)
 #
 # sort elements by increasing isave
 #
-saves = np.sort(saves, order='isave')
+saves = np.sort(saves, order=['isave', 'variable'])
 #
 # harvest some information from the log file
 #
