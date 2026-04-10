bc7cd9b175db651da2ce1b0fbededd9410ae16f2
Pedro Costa <p.simoes.costa@gmail.com>
2026-04-10 12:37:39 +0200

    More consistent organization of submodules.

--- dependencies/external.mk
+++ dependencies/external.mk
@@ -1,25 +1,28 @@
 #
 # external libraries compilation
 #
+D2D_MAKEFILE := ../configs/2decomp-fft/Makefile
+DIEZ_MAKEFILE := ../configs/diezDecomp/Makefile
+
 ifeq ($(strip $(GPU)),1)
 ifneq ($(strip $(USE_DIEZDECOMP)),1)
 libs: $(wildcard $(LIBS_DIR)/2decomp-fft/src/*.f90)
-	cd $(LIBS_DIR)/2decomp-fft && make
+	$(MAKE) -C $(LIBS_DIR)/2decomp-fft -f $(D2D_MAKEFILE)
 	cd $(LIBS_DIR)/cuDecomp && mkdir -p build && cd build && cmake .. && make -j
 libsclean: $(wildcard $(LIBS_DIR)/2decomp-fft/src/*.f90)
-	cd $(LIBS_DIR)/2decomp-fft && make clean
+	$(MAKE) -C $(LIBS_DIR)/2decomp-fft -f $(D2D_MAKEFILE) clean
 	cd $(LIBS_DIR)/cuDecomp/build && make clean; cd .. && rm -rf build
 else
 libs: $(wildcard $(LIBS_DIR)/2decomp-fft/src/*.f90)
-	cd $(LIBS_DIR)/2decomp-fft && make
-	cd $(LIBS_DIR)/diezDecomp && make -j
+	$(MAKE) -C $(LIBS_DIR)/2decomp-fft -f $(D2D_MAKEFILE)
+	$(MAKE) -C $(LIBS_DIR)/diezDecomp -f $(DIEZ_MAKEFILE) -j
 libsclean: $(wildcard $(LIBS_DIR)/2decomp-fft/src/*.f90)
-	cd $(LIBS_DIR)/2decomp-fft && make clean
-	cd $(LIBS_DIR)/diezDecomp && make clean
+	$(MAKE) -C $(LIBS_DIR)/2decomp-fft -f $(D2D_MAKEFILE) clean
+	$(MAKE) -C $(LIBS_DIR)/diezDecomp -f $(DIEZ_MAKEFILE) clean
 endif
 else
 libs: $(wildcard $(LIBS_DIR)/2decomp-fft/src/*.f90)
-	cd $(LIBS_DIR)/2decomp-fft && make
+	$(MAKE) -C $(LIBS_DIR)/2decomp-fft -f $(D2D_MAKEFILE)
 libsclean: $(wildcard $(LIBS_DIR)/2decomp-fft/src/*.f90)
-	cd $(LIBS_DIR)/2decomp-fft && make clean
+	$(MAKE) -C $(LIBS_DIR)/2decomp-fft -f $(D2D_MAKEFILE) clean
 endif
